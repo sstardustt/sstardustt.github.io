7ef1969ad1e17cf3a4fff5dd788a7adf5941ba88
David <david.alves0710@gmail.com>
2025-11-14 23:13:35 -0300

    Add star generation to stars.js

--- stars.js
+++ stars.js
@@ -1,1 +1,15 @@
+const starCount = 180;
+
+for (let i = 0; i < starCount; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+
+
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.top = Math.random() * 100 + "vh";
+
+    star.style.animationDelay = Math.random() * 4 + "s";
+
+    document.body.appendChild(star);
+}
 
